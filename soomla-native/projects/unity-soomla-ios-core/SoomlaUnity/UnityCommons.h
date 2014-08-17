74aaf5b4abd1773975e74ca16a4318cf90c47dfb
refaelos <refaeldakar@gmail.com>
2014-08-17 16:08:12 +0300

    moved a common function to common file

--- soomla-native/projects/unity-soomla-ios-core/SoomlaUnity/UnityCommons.h
+++ soomla-native/projects/unity-soomla-ios-core/SoomlaUnity/UnityCommons.h
@@ -2,3 +2,13 @@
 #define EXCEPTION_ITEM_NOT_FOUND 	 -1
 #define EXCEPTION_INSUFFICIENT_FUNDS -2
 #define EXCEPTION_NOT_ENOUGH_GOODS	 -3
+
+char* Soom_AutonomousStringCopy (const char* string)
+{
+    if (string == NULL)
+        return NULL;
+    
+    char* res = (char*)malloc(strlen(string) + 1);
+    strcpy(res, string);
+    return res;
+}
