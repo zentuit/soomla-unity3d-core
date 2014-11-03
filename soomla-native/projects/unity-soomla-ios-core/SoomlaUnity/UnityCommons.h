50d2659bca91e0ec8ef06624b7c592dde3a18319
refaelos <refaeldakar@gmail.com>
2014-11-03 10:46:00 +0200

    Fixed an issue with a duplicated function and bridge project config

--- soomla-native/projects/unity-soomla-ios-core/SoomlaUnity/UnityCommons.h
+++ soomla-native/projects/unity-soomla-ios-core/SoomlaUnity/UnityCommons.h
@@ -1,12 +1,5 @@
 #define NO_ERR						 0
 
-char* Soom_AutonomousStringCopy (const char* string)
-{
-    if (string == NULL)
-        return NULL;
-    
-    char* res = (char*)malloc(strlen(string) + 1);
-    strcpy(res, string);
-    return res;
-}
+char* Soom_AutonomousStringCopy (const char* string);
+
 
